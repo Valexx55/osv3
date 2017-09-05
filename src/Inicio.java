c7c19bb329977f72bff8ead640f8f1707d628dbd
root <root@debian.vale>
2017-09-05 18:48:03 +0200

    cambios adding dependencias

--- src/Inicio.java
+++ src/Inicio.java
@@ -32,12 +32,5 @@ public class Inicio extends HttpServlet {
 		response.sendRedirect("holamundo.html");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 }
